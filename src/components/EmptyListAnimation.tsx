0dd18ac424ea52115dffbd0bc5cd8e22c9a0ea82
Rutarenzi <rutagaramaaxcel@gmail.com>
2024-01-11 15:37:13 +0200

    adding unfinished cart

---
+++ src/components/EmptyListAnimation.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Lottie from 'lottie-react-native';
+interface EmptyListAnimation {
+    title: string;
+}
+const EmptyListAnimation: React.FC<EmptyListAnimation> = ({title}) => {
+  return (
+    <View style={styles.EmptyCardContainer}>
+      {/* <Lottie
+      style={styles.LottieStyle} 
+      source={require('../lottie/coffeecup.json')}
+      autoPlay
+      loop
+      /> */}
+      <Text>{title}</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    EmptyCardContainer: {
+        flex:1,
+        justifyContent: 'center',
+    },
+    LottieStyle: {
+        height: 300,
+    }
+})
+export default EmptyListAnimation
