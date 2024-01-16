431e82feca86790ab871bc7f8c0ff1cd5bacb1f2
Rutarenzi <rutagaramaaxcel@gmail.com>
2024-01-16 18:08:38 +0200

    Boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Coffee App created By Axcel!</Text>
+      <Text>Boilerplate Created by Axcel!</Text>
       <StatusBar style="auto" />
     </View>
   )
